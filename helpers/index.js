3c042a56d527ca3b1c6116b59df1649ec1c2add6
Elad Shaham <7040645+eshaham@users.noreply.github.com>
2023-06-01 09:51:47 +0300

    fix position url schema and strip http

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,15 @@
 // Helpers
 const prettyDate = require('./pretty_date');
 
+function domainOnly(url) {
+  if (!url) {
+    return url;
+  }
+  return url.replace(/(https?:\/\/)?(www.)?/i, '');
+}
+
 // Exports
 module.exports = {
   prettyDate,
+  domainOnly,
 };
